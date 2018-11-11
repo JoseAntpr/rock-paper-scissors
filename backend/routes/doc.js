6c567c5c39a7be37ef2c46323ae0e8cbd9d76566
José Antonio Palacios Ramírez <joseantpalaciosramirez@gmail.com>
2018-11-11 11:40:21 +0100

    ADD: doc page for api

--- backend/routes/doc.js
+++ backend/routes/doc.js
@@ -3,8 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res, next) => {
-
-    res.redirect("https://documenter.getpostman.com/view/1688848/RWgqWzJk");
+    res.redirect('https://documenter.getpostman.com/view/1688848/RzZ9Gekn');
 });
 
 module.exports = app;
